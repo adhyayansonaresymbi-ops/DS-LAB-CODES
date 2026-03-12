d68400db057c6f76da586d939c5247e202c22e5b
adhyayansonaresymbi-ops <adhyayan.sonare.batch2025@sitnagpur.siu.edu.in>
2026-03-12 14:34:27 +0530

    Update Concatenate_Operation_Single_LinkedList.c

--- Experiment4/Concatenate_Operation_Single_LinkedList.c
+++ Experiment4/Concatenate_Operation_Single_LinkedList.c
@@ -1,52 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 struct node {
-    NODE newNode = createNode(x);
+    int data;
+    struct node* next;
+};
 
-    if(first == NULL)
-        return newNode;
+typedef struct node* NODE;
 
-    NODE temp = first;
-    while(temp->next != NULL)
-        temp = temp->next;
+int main() {
 
-    temp->next = newNode;
+    NODE t1 = NULL;
+    NODE t2 = NULL;
+    NODE newNode, temp;
+    int n1, n2, x;
 
-    return first;    
-    
-    
-    
-}
+    printf("Enter number of elements in List 1: ");
+    scanf("%d", &n1);
 
-NODE concatenate(NODE t1, NODE t2) {
+    printf("Enter elements of List 1:\n");
+    for(int i = 0; i < n1; i++) {
+        scanf("%d", &x);
+
+        newNode = (NODE)malloc(sizeof(struct node));
+        if(newNode == NULL){
+            printf("Memory allocation failed\n");
+            exit(1);
+        }
+
+        newNode->data = x;
+        newNode->next = NULL;
+
+        if(t1 == NULL)
+            t1 = newNode;
+        else {
+            temp = t1;
+            while(temp->next != NULL)
+                temp = temp->next;
+            temp->next = newNode;
+        }
+    }
+
+    printf("Enter number of elements in List 2: ");
+    scanf("%d", &n2);
+
+    printf("Enter elements of List 2:\n");
+    for(int i = 0; i < n2; i++) {
+        scanf("%d", &x);
+
+        newNode = (NODE)malloc(sizeof(struct node));
+        if(newNode == NULL){
+            printf("Memory allocation failed\n");
+            exit(1);
+        }
+
+        newNode->data = x;
+        newNode->next = NULL;
+
+        if(t2 == NULL)
+            t2 = newNode;
+        else {
+            temp = t2;
+            while(temp->next != NULL)
+                temp = temp->next;
+            temp->next = newNode;
+        }
+    }
+
+    /* Concatenate Lists */
     if(t1 == NULL)
-        return t2;
+        t1 = t2;
+    else {
+        temp = t1;
+        while(temp->next != NULL)
+            temp = temp->next;
+        temp->next = t2;
+    }
 
-    if(t2 == NULL)
-        return t1;
-
-    NODE temp = t1;
-    while(temp->next != NULL)
-        temp = temp->next;
-
-    temp->next = t2;
-
-
-    return t1;    
-    
-    
-    
-}
-
-void displayList(NODE first) {
-    NODE temp = first;
-
-    while(temp != NULL) {
+    /* Display List */
+    printf("Concatenated List:\n");
+    temp = t1;
+    while(temp != NULL){
         printf("%d --> ", temp->data);
         temp = temp->next;
     }
+    printf("NULL\n");
 
-
-    printf("NULL\n");    
-    
-    
-    
+    return 0;
 }
